7371316a330dfa6dce4ccd76b969f915302938a0
RafaelSGil <rafaelgil131@gmail.com>
2025-12-09 19:45:32 +0000

    feat(api): implements endpoint to get a user historical data

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -43,6 +43,8 @@ import {
   ValidationError,
 } from './errors'
 import JSONbig from 'json-bigint'
+import { getOrdersByAddressKeys } from '@open-djed/db'
+import type { AddressKey } from '@open-djed/db/src/types'
 
 //NOTE: We only need this cache for transactions, not for other requests. Using this for `protocol-data` sligltly increases the response time.
 const requestCache = new TTLCache<string, { value: Response; expiry: number }>({ ttl: 10_000 })
@@ -175,6 +177,16 @@ async function completeTransaction(createOrderFn: () => TxBuilder) {
       }
     }
     throw err
+  }
+}
+
+function getAddressKey(addr: string): AddressKey {
+  try {
+    const paymentKeyHash = paymentCredentialOf(addr).hash
+    const stakeKeyHash = stakeCredentialOf(addr).hash
+    return { paymentKeyHash, stakeKeyHash }
+  } catch {
+    return { paymentKeyHash: '', stakeKeyHash: '' }
   }
 }
 
@@ -356,15 +368,7 @@ const app = new Hono()
       try {
         const allPendingOrders = await getOrderUTxOs()
 
-        const usedAddressesKeys = json.usedAddresses.map((addr) => {
-          try {
-            const paymentKeyHash = paymentCredentialOf(addr)
-            const stakeKeyHash = stakeCredentialOf(addr)
-            return { paymentKeyHash: paymentKeyHash.hash, stakeKeyHash: stakeKeyHash.hash }
-          } catch {
-            return { paymentKeyHash: '', stakeKeyHash: '' }
-          }
-        })
+        const usedAddressesKeys = json.usedAddresses.map(getAddressKey)
 
         const filteredPendingOrders = allPendingOrders.filter((order) =>
           usedAddressesKeys.some(
@@ -472,6 +476,72 @@ const app = new Hono()
         }
         console.error('Unhandled error:', err)
         return c.json({ error: 'InternalServerError', message: 'Something went wrong.' }, 500)
+      }
+    },
+  )
+  .post(
+    '/historical-orders',
+    cacheMiddleware,
+    describeRoute({
+      description: 'Get the historical orders',
+      tags: ['Action'],
+      responses: {
+        200: {
+          description: 'Successfully got the user historical orders',
+          content: {
+            'text/plain': {
+              example: 'Order',
+            },
+          },
+        },
+        400: {
+          description: 'Bad Request',
+          content: {
+            'text/plain': {
+              example: 'Bad Request',
+            },
+          },
+        },
+        500: {
+          description: 'Internal Server Error',
+          content: {
+            'text/plain': {
+              example: 'Internal Server Error',
+            },
+          },
+        },
+      },
+    }),
+    zValidator('json', z.object({ usedAddresses: z.array(z.string()) })),
+    async (c) => {
+      let json
+
+      try {
+        json = c.req.valid('json')
+        if (!json?.usedAddresses) {
+          throw new ValidationError('Missing hexAddress in request.')
+        }
+      } catch (e) {
+        console.error('Invalid or missing request payload.', e)
+        throw new ValidationError('Invalid or missing request payload.')
+      }
+
+      try {
+        const usedAddressesKeys = json.usedAddresses.map(getAddressKey)
+
+        const orders = await getOrdersByAddressKeys(usedAddressesKeys)
+
+        console.log('Order:', orders)
+
+        return new Response(JSONbig.stringify({ orders: orders }), {
+          headers: { 'Content-Type': 'application/json' },
+        })
+      } catch (err) {
+        if (err instanceof AppError) {
+          throw err
+        }
+        console.error('Unhandled error in orders endpoint:', err)
+        throw new InternalServerError()
       }
     },
   )
