b42b47db1629eceacdcaae050faff8cab09689d9
RafaelSGil <rafaelgil131@gmail.com>
2025-12-12 14:03:11 +0000

    fix(app): fixes build errors

--- packages/app/src/lib/loader.ts
+++ packages/app/src/lib/loader.ts
@@ -4,7 +4,7 @@ export function getLoaderData(): Env {
   const API_URL = process.env.NEXT_PUBLIC_API_URL
   const NETWORK = process.env.NEXT_PUBLIC_NETWORK as 'Preprod' | 'Mainnet' | undefined
   const CONFIG = process.env.NEXT_PUBLIC_CONFIG
-  const POSTHOG = process.env.NEXT_PUBLIC_POSTHOG_API_KEY
+  const POSTHOG = '' //process.env.NEXT_PUBLIC_POSTHOG_API_KEY
 
   if (!API_URL || !NETWORK || !CONFIG || !POSTHOG) {
     throw new Error('Missing environment variables')
