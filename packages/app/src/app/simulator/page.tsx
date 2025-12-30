3a9d03982323f06b18cd5b8b6e6dc91cb10c9a8a
Nun0Santos <nunosantos2018@hotmail.com>
2025-12-30 14:33:21 +0000

    feat: add what is a yield simulator

--- packages/app/src/app/simulator/page.tsx
+++ packages/app/src/app/simulator/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from "react"
 import Button from "@/components/Button"
+import Modal from "@/components/modals/Modal"
+import SimulatorContent from "@/components/SimulatorContent"
 
 export default function SimulatorPage() {
-  const [, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
     <div className="desktop:pt-32 desktop:pb-64 mx-auto w-full max-w-280 pt-16 pb-16">
@@ -26,6 +28,15 @@ export default function SimulatorPage() {
           />
         </div>
       </div>
+      
+      {/* Modal */}
+      <Modal
+        title="What is a yield simulator?"
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        <SimulatorContent />
+      </Modal>
     </div>
   )
 }
---
+++ packages/app/src/components/SimulatorContent.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image"
+
+const ITEMS = [
+  {
+    text: "Fees you pay when buying and selling",
+    icon: "/logos/5.png",
+    name: "5",
+  },
+  {
+    text: "ADA rewards you earn while holding SHEN",
+    icon: "/logos/rewards.png",
+    name: "rewards",
+  },
+  {
+    text: "Fees earned from minting and burning activity in the protocol",
+    icon: "/logos/fees.png",
+    name: "fees",
+  },
+  {
+    text: "Profit or loss from ADA price movement",
+    icon: "/logos/buying-selling.png",
+    name: "buying-selling",
+  },
+  {
+    text: "Your total estimated PNL",
+    icon: "/logos/1.png",
+    name: "1",
+  },
+]
+
+const SimulatorContent = () => {
+  return (
+    <div className="text-secondary flex flex-col gap-16 text-sm">
+      <div className="flex flex-col gap-16">
+        <p className="gap-16 text-sm">
+          The Yield Simulator helps you understand how profitable a SHEN
+          position could be. By entering the amount of SHEN, your buy and sell
+          dates, and ADA prices, the tool estimates:
+        </p>
+
+        <ul className="flex flex-col gap-16">
+          {ITEMS.map(({ text, icon, name }) => (
+            <li key={text} className="flex items-center gap-16">
+              <Image
+                src={icon}
+                alt={`${name}-picture`}
+                width={40}
+                height={40}
+              />
+              <span>{text}</span>
+            </li>
+          ))}
+        </ul>
+
+        <p>
+          It's not a prediction tool - it's a calculator that shows how your
+          returns change based on the inputs you choose, so you can plan your
+          strategy with clarity.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SimulatorContent
