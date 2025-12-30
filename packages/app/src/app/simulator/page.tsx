684da849d25d2c31865568a0bbbcc2a27d795c1d
Nun0Santos <nunosantos2018@hotmail.com>
2025-12-30 11:49:14 +0000

    feat: add initial simulator page

---
+++ packages/app/src/app/simulator/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useState } from "react"
+import Button from "@/components/Button"
+
+export default function DashboardPage() {
+  const [, setIsModalOpen] = useState(false)
+
+  return (
+    <div className="desktop:pt-32 desktop:pb-64 mx-auto w-full max-w-280 pt-16 pb-16">
+      {/* Header */}
+      <div className="desktop:flex-row desktop:items-end flex flex-col justify-between gap-12">
+        <div className="flex flex-col gap-6">
+          <h2 className="text font-bold">SHEN Yield Simulator</h2>
+          <span className="text-secondary text-sm">
+            Check the yield outcomes quickly and clearly
+          </span>
+        </div>
+
+        <div>
+          <Button
+            variant="text"
+            size="medium"
+            text="What is a yield simulator?"
+            onClick={() => setIsModalOpen(true)}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
