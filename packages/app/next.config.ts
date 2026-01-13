811cad24fb958219850eee5659b66363f3bc4c82
pmsnnogueira <pmsn.mig2002@gmail.com>
2026-01-13 12:28:52 +0000

    chore: fix svgr webpack next config

--- packages/app/next.config.ts
+++ packages/app/next.config.ts
@@ -14,6 +14,29 @@ const nextConfig: NextConfig = {
       type: "webassembly/async",
     })
 
+    // SVG loader configuration
+
+    // @ts-expect-error: fileLoaderRule exists
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
     return config
   },
   turbopack: {
