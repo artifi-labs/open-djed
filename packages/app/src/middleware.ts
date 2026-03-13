42dbb7ed8e1a212cd3efb9e45c3d49a6cdd430d1
pmsnnogueira <pmsn.mig2002@gmail.com>
2026-03-13 15:00:20 +0000

    chore(app): change deprecated middleware file

--- packages/app/src/middleware.ts
+++ packages/app/src/middleware.ts
@@ -1,15 +1,11 @@
 import createMiddleware from "next-intl/middleware"
 import { routing } from "@/i18n/routing"
 
-const intlMiddleware = createMiddleware(routing)
-
-export function middleware(request: Parameters<typeof intlMiddleware>[0]) {
-  return intlMiddleware(request)
-}
+export default createMiddleware(routing)
 
 export const config = {
   // match all paths except for the ones starting with
-  // - /api, /_next/static, /_next/image, and some common static file extensions
+  // - api, _next/static, _next/image, and some common files like favicon.ico, sitemap.xml, and robots.txt
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)",
   ],
