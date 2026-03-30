a261e25d19de9686fdeeb6be9148fe97a0a1a123
pmsnnogueira <pmsn.mig2002@gmail.com>
2026-03-30 16:46:34 +0100

    chore(app): add fr translations

--- packages/app/src/i18n/settings.ts
+++ packages/app/src/i18n/settings.ts
@@ -1,4 +1,4 @@
 export const fallbackLng = "en"
-export const languages = [fallbackLng, "pt"]
+export const languages = [fallbackLng, "pt", "fr"]
 export const defaultNS = "translation"
 export const LOOKUP_I8N_LOCAL_STORAGE = "i18Lng"
--- packages/app/src/lib/constants.ts
+++ packages/app/src/lib/constants.ts
@@ -18,6 +18,7 @@ export const CARDANOSCAN_BASE_URL = `https://${env.NETWORK === "Preprod" ? env.N
 export const SUPPORTED_LANGUAGES = [
   { code: "en", label: "English" },
   { code: "pt", label: "Português" },
+  { code: "fr", label: "Français (French)" },
 ]
 
 export const ORDERS_PER_PAGE = 10
