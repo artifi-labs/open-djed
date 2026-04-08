3d74b7916e06da1dc801451e1ef5e1f4d0d28544
pmsnnogueira <pmsn.mig2002@gmail.com>
2026-04-08 15:49:16 +0100

    fix(blockfrost): add missing evaluateTx function override

--- packages/blockfrost/src/schemas/block/latestblock.schema.ts
+++ packages/blockfrost/src/schemas/block/latestblock.schema.ts
@@ -10,13 +10,13 @@ export const latestBlockSchema = z.object({
   slot_leader: z.string(),
   size: z.number(),
   tx_count: z.number(),
-  output: z.string(),
-  fees: z.string(),
+  output: z.string().nullable(),
+  fees: z.string().nullable(),
   block_vrf: z.string(),
-  op_cert: z.string(),
-  op_cert_counter: z.number(),
+  op_cert: z.string().nullable(),
+  op_cert_counter: z.string().nullable(),
   previous_block: z.string(),
-  next_block: z.string(),
+  next_block: z.string().nullable(),
   confirmations: z.number(),
 })
 
