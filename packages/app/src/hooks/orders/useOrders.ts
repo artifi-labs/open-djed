275e0cc5fd268e55ce55b32cf0145b0abfcac151
pmsnnogueira <pmsn.mig2002@gmail.com>
2026-04-10 17:48:49 +0100

    fix(app): fix orders request without usedAddresses

--- packages/app/src/hooks/orders/useOrders.ts
+++ packages/app/src/hooks/orders/useOrders.ts
@@ -22,11 +22,14 @@ export const useOrders = ({ queryParams }: UseOrdersParams = {}) => {
     enabled: !!wallet,
   })
 
+  const hasAddresses = usedAddresses.length > 0
+
   const { data, isLoading, isError, error, refetch } = useOrdersQuery({
     body: {
       usedAddresses,
     },
     queryParams: params,
+    options: { enabled: hasAddresses },
   })
 
   const setFilterStatus = (value: OrderStatus[] | undefined) => {
